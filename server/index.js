4d29b358a438b7515e0d3c0092b8e66d63052fee
aayushchauhan10 <ai.aayush10@gmail.com>
2023-03-15 10:16:30 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/image", imageRoutes);
 
+//api end point
 app.get("/", async (req, res) => {
   res.send("Hello from Ayush Chauhan");
 });
